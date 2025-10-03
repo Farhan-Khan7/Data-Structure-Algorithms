f6811acc977e1e00303b9032dab6ca15065f72bb
Farhan-Khan7 <fk5518744@gmail.com>
2025-10-04 01:06:13 +0530

    I have done my digit count code

--- DigitsCount.cpp
+++ DigitsCount.cpp
@@ -4,21 +4,32 @@ using namespace std;
 
 int main(){
 
+    // Approach 1
+
     string number;
     cout<<"Enter the numbers : ";
     getline(cin , number);
-
+    
+    int digitsCount = 0;
     stringstream num(number);
     int n;
 
-    int digitsCount = 0;
     for(char ch : number){
         if(isdigit(ch)){
             digitsCount++;
         }
     }
-    cout<<"Total Digits : "<<digitsCount<<endl;
+    //  Approach 2
+    int numb;
+    cout<<"Enter Your Number = ";
+    cin>>numb;
+
+    while (numb != 0){
+        numb = numb/10;
+        digitsCount++;
+    }
+
+    // cout << "Total Digits : " << digitsCount << endl;
 
     return 0;
-
 }
